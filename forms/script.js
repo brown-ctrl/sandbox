50c0fbb55d3428cb53d6fd3b4823d2bcd5dd92a5
Brown Memoye <memoyebrown@gmail.com>
2023-04-24 11:54:37 +0100

    add files

---
+++ forms/script.js
@@ -0,0 +1,48 @@
+// alert("Welcome to JavaScript!");
+// prompt("Hello world", "Fine, thank you");
+// console.log("Hello world");
+// console.error("find error");
+
+// this is a single -line comment
+
+/* This is a 
+    multiline comment */
+
+// #Variables
+let age = 22;;
+var userName = "";
+const pi = 3.142;
+let understood = true;
+let nullVar = null;
+let undefinedVar = undefined;
+
+console.log(typeof age);
+// console.log(userName == false);
+console.log(typeof pi);
+console.log(typeof understood);
+console.log(typeof nullVar);
+console.log(typeof undefinedVar);
+
+// #Operators
+/*
+    Arithmetic: + - * /
+                ** means raised to the power
+                ++       increment
+                %        Modulus
+                --       Decrement
+    Assignment: =  +=  -=  *=  /=
+    Comparison: ==  ===  !=  !==  >  <  >=  <=
+    Logical:    &&  ||   
+*/
+
+
+// let x = 2;
+// // console.log(++x);
+// // console.log(x++);
+// // console.log(x);
+// console.log(2 ** 2 ** 3 * 3);
+// console.log(5==7);
+
+console.log(3 > 2 > 1 === false);
+console.log();
+
